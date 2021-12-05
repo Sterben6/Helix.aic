b139d892b6fd5f914f9672d5cb0d14c697679985
Sterben <oreozgamingofficial@gmail.com>
2021-12-04 22:05:03 -0600

    Initial commit.

--- src/api/setrank/main.ts
+++ src/api/setrank/main.ts
@@ -1,4 +1,5 @@
 import { Route, Server } from '../../classes';
+import noblox from 'noblox.js';
 
 export default class app extends Route {
 
@@ -11,12 +12,22 @@ export default class app extends Route {
 
     public bind() {
         this.router.post('/', async (req, res) => {
-            console.log('called');
-            console.log("body: " + JSON.stringify(req.body));
-            console.log(req.headers['test']);
-            console.log(req.headers);
-            if (req.headers.test == "00") {
-                console.log('test')
+            if (!req.headers.token)
+                return res.status(401).json({ code: this.constants.codes.UNAUTHORIZED, message: this.constants.messages.PERMISSION_DENIED});
+            if (!req.headers.group || !req.headers.userid || !req.headers.rank || !req.headers.reason)
+                return res.status(400).json({ code: this.constants.codes.CLIENT_ERROR, message: this.constants.messages.CLIENT_ERROR});
+            if (req.headers.token !== "e61e70d9-29ae-4dbc-8fc4-5b9fa2d6f470")
+                return res.status(401).json({ code: this.constants.codes.UNAUTHORIZED, message: this.constants.messages.PERMISSION_DENIED});
+            const group = req.headers.group;
+            const userid = req.headers.userid;
+            const rank = req.headers.rank;
+            const reason = req.headers.reason;
+
+            try {
+                await noblox.setRank(group, userid, rank);
+                return res.status(200).json({ code: this.constants.codes.SUCCESS, message: this.constants.messages.SUCCESS});
+            } catch (e) {
+                return res.status(500).json({ code: this.constants.codes.SERVER_ERROR, message: this.constants.messages.SERVER_ERROR});
             }
         })
     }
