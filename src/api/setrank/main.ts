0d5df747f76bb73e3ad0ee3add2176a769d50313
Sterben <oreozgamingofficial@gmail.com>
2021-12-04 20:26:52 -0600

    Initial commit.

--- src/api/setrank/main.ts
+++ src/api/setrank/main.ts
@@ -12,7 +12,7 @@ export default class app extends Route {
     public bind() {
         this.router.post('/', async (req, res) => {
             console.log('called');
-            console.log("body: " + req.body.toString());
+            console.log("body: " + JSON.stringify(req.body));
             console.log(req.headers['Test']);
             console.log(req.headers);
             if (req.headers.Test == "00") {
